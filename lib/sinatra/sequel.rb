9eafcb802c1786becde84b7d02561b8694b80a66
Dana Scheider <dana.scheider@gmail.com>
2014-09-04 14:21:19 -0700

    Add documentation

--- lib/sinatra/sequel.rb
+++ lib/sinatra/sequel.rb
@@ -2,6 +2,30 @@ require 'time'
 require 'sinatra/base'
 require 'sequel'
 
+# Sinatra::SequelExtension was forked from (rtomayko's sinatra-sequel gem)[https://github.com/rtomayko/sinatra-sequel],
+# which has not had any commits since 2013. Install this gem using 
+# +sudo gem install sinatra-sequel_extension+ or (if using RVM) 
+# +gem install sinatra-sequel_extension+. rtomayko's gem can be installed
+# with +sudo gem install sinatra-sequel+. The following information pertains to 
+# this gem only:
+#
+# Author::     Ryan Tomayko
+# Maintainer:: Dana Scheider
+# Copyright::  Copyright (c) 2009 Ryan Tomayko
+# License::    Distributes under the MIT License
+#
+# This gem extends the Sinatra DSL to work with the Sequel ORM gem. Install
+# the Sinatra and Sequel gems using +sudo gem install sinatra sequel+, or 
+# +gem install sinatra sequel+ if using RVM. Alternatively, add the following
+# to your app's Gemfile[http://bundler.io]:
+#     gem 'sinatra'
+#     gem 'sequel'
+#     gem 'sequel-sinatra_extension', '>= 0.9'
+# Install the gems using +bundle install+.
+#
+# Sinatra::SequelExtension can work with MySQL, PostgreSQL, and SQLite3 adapters.
+# You will need to include the appropriate database gem in your Gemfile as well.
+
 module Sinatra
   module SequelHelper
     def database
